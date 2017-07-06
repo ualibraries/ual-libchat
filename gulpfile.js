54321ac57712ba5dd9ed9283635aff0d2a7bae09
Christopher Green <cg433n@email.arizona.edu>
2017-07-06 14:53:57 -0700

    More scaffolding

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
+const connect = require('gulp-connect')
+const localtunnel = require('localtunnel')
 const postcss = require('gulp-postcss')
 const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('css', () => {
   return gulp.src('./src/*.css')
+    .pipe(sourcemaps.init())
     .pipe(postcss())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'))
+})
+
+gulp.task('serve', () => {
+  const tunnel = localtunnel(8000, (err, tunnel) => {
+    if (err) {
+      console.error(err)
+    }
+
+    console.log(`\nStatic server running at ${tunnel.url}\n`)
+    console.log(`Copy the following css into the 'Custom CSS' field:`)
+    console.log(`@import url("${tunnel.url}/ual-libchat.css");\n`)
+  })
+
+  tunnel.on('close', () => {
+    console.log(`Tunnels are closed`)
+  })
+
+  connect.server({
+    root: 'css',
+    port: 8000
+  })
+})
+
+gulp.task('watch', ['serve'], () => {
+  gulp.watch('./src/**/*.css', ['css'])
 })
 
 gulp.task('default', ['css'])
--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'stylelint': {configFile: '.stylelintrc.json'},
     'postcss-import': {},
     'postcss-cssnext': {},
+    'cssnano': {preset: 'default', autoprefixer: false},
     'postcss-reporter': {clearMessages: true}
   }
 }
