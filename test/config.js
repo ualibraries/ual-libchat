5d106c877fa3a0be79107c6a8911c3bf1017ae8e
Christopher Green <cg433n@email.arizona.edu>
2017-07-13 09:54:50 -0700

    Updates chat test page

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testURL: 'https://v2.libanswers.com/chati.php?hash=e7fa5c4fdcde97a650b8acc74bb1fc9d',
+  testURL: 'https://v2.libanswers.com/chati.php?hash=07713bc057f66ebcdccd4dd1b4a2be3e',
   stylesheetImport: `@import url("https://www.library.arizona.edu/vendor-support/libchat/current/css/ual-libchat.css");`,
   theirCSSURL: 'https://v2.libanswers.com/css2.15.3/LibAnswers_widget.css'
 }
