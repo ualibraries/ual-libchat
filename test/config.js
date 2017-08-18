1e06b66bafdf29d297b7e7f894d5e65cdc3b5131
Christopher Green <cg433n@email.arizona.edu>
2017-08-18 15:35:20 -0700

    Bumped LibChat stylesheet version

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testURL: 'https://v2.libanswers.com/chati.php?hash=07713bc057f66ebcdccd4dd1b4a2be3e',
   stylesheetImport: `@import url("https://www.library.arizona.edu/vendor-support/libchat/current/css/ual-libchat.css");`,
-  theirCSSURL: 'https://v2.libanswers.com/css2.15.5/LibAnswers_widget.css'
+  theirCSSURL: 'https://v2.libanswers.com/css2.15.7/LibAnswers_widget.css'
 }
