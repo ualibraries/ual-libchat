c3151b0387b63802d813eb1bd66fa8557728d944
Christopher Green <cg433n@email.arizona.edu>
2017-10-16 12:26:24 -0700

    Bumps stylesheet version to 2.15.9, fixes failing test

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testURL: 'https://v2.libanswers.com/chati.php?hash=07713bc057f66ebcdccd4dd1b4a2be3e',
   stylesheetImport: `@import url("https://www.library.arizona.edu/vendor-support/libchat/current/css/ual-libchat.css");`,
-  theirCSSURL: 'https://v2.libanswers.com/css2.15.8/LibAnswers_widget.min.css'
+  theirCSSURL: 'https://v2.libanswers.com/css2.15.9/LibAnswers_widget.min.css'
 }
