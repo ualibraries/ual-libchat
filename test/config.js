f23a9b47dff19f41dde5ec68fa8c47ae4dac7783
Christopher Green <cg433n@email.arizona.edu>
2018-08-15 13:18:34 -0700

    Bumpse LibChat css version number

--- test/config.js
+++ test/config.js
@@ -3,5 +3,5 @@ module.exports = {
     'https://v2.libanswers.com/chati.php?hash=07713bc057f66ebcdccd4dd1b4a2be3e',
   stylesheetImport:
     '@import url("https://ualibr-libchat.s3.amazonaws.com/ual-libchat.css");',
-  theirCSSURL: 'https://v2.libanswers.com/css2.21.2/LibAnswers_widget.min.css'
+  theirCSSURL: 'https://v2.libanswers.com/css2.22.4/LibAnswers_widget.min.css'
 }
