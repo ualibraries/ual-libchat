e34efab76897da14044a2e0014657a41f93e09e1
Christopher Green <cg433n@email.arizona.edu>
2018-10-31 11:21:03 -0700

    Bumps LibChat CSS version number

--- test/config.js
+++ test/config.js
@@ -3,5 +3,5 @@ module.exports = {
     'https://v2.libanswers.com/chati.php?hash=07713bc057f66ebcdccd4dd1b4a2be3e',
   stylesheetImport:
     '@import url("https://ualibr-libchat.s3.amazonaws.com/ual-libchat.css");',
-  theirCSSURL: 'https://v2.libanswers.com/css2.23.3/LibAnswers_widget.min.css'
+  theirCSSURL: 'https://v2.libanswers.com/css2.23.4/LibAnswers_widget.min.css'
 }
